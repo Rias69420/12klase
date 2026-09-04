1f2326e97225ca92a4158faacdde69d3ed894938
Rias69420 <110286536+Rias69420@users.noreply.github.com>
2026-09-04 15:56:09 +0300

    Add files via upload

---
+++ intro.sql
@@ -0,0 +1,23 @@
+CREATE TABLE SKOLENI (
+    ID INTEGER PRIMARY KEY,
+    Vards TEXT NOT NULL,
+    Uzvards TEXT NOT NULL,
+    Vecums INTEGER NOT NULL,
+    Dzim_diena DATE NOT NULL
+);
+
+INSERT INTO SKOLENI (ID, Vards, Uzvards, Vecums, Dzim_diena)
+VALUES
+(1, 'Jānis', 'Krūmiņš', 18, '2008-02-29'),
+(2, 'Anna', 'Bērziņa', 18, '2008-01-01');
+
+INSERT INTO SKOLENI (ID, Vards, Uzvards, Vecums, Dzim_diena) VALUES
+(3, 'Pēteris', 'Neuzvārdots', 15, '2011-03-03');
+
+SELECT * FROM SKOLENI;
+
+WHERE Vards = 'Anna' AND Uzvards = 'Bērziņa';
+
+
+DROP TABLE SKOLENI;
+
